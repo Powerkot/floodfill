4b8399e382d98bd7b528bd61f0b2ea5dc4fcb2ab
Denis Moklyakov <denis.moklyakov@ramax.ru>
2019-05-16 18:33:34 +0300

    add main activity + code style fix

---
+++ app/src/main/java/localhost/dmoklyakov/floodfill/FloodFillView.kt
@@ -0,0 +1,2 @@
+package localhost.dmoklyakov.floodfill
+
---
+++ app/src/main/java/localhost/dmoklyakov/floodfill/MainActivity.kt
@@ -0,0 +1,11 @@
+package localhost.dmoklyakov.floodfill
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
