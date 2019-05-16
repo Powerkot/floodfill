8e6a44e16cbd355df3798d91fd881001f15ed08f
Denis Moklyakov <denis.moklyakov@ramax.ru>
2019-05-16 20:29:49 +0300

    Very simple flood fill view

--- app/src/main/java/localhost/dmoklyakov/floodfill/FloodFillView.kt
+++ app/src/main/java/localhost/dmoklyakov/floodfill/FloodFillView.kt
@@ -1,2 +1,38 @@
 package localhost.dmoklyakov.floodfill
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import java.util.*
+
+/**
+ * Simple and stupid view for testing algorithms
+ */
+class FloodFillView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+
+    private var paint = Paint()
+    var image: Array<BitSet>? = null
+
+    init {
+        paint.isAntiAlias = false
+        paint.color = 0xFFFFFFFF.toInt()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawColor(0xFF000000.toInt())
+        image?.let {
+            val sizeX = 1f * width / it.size
+            val sizeY = 1f * height / it[0].length()
+            for (y in 0 until it.size) {
+                for (x in 0 until it[y].length()) {
+                    if (it[y][x]) {
+                        canvas.drawRect(x * sizeX, y * sizeY, (x + 1) * sizeX, (y + 1) * sizeY, paint)
+                    }
+                }
+            }
+        }
+    }
+
+}
--- app/src/main/java/localhost/dmoklyakov/floodfill/MainActivity.kt
+++ app/src/main/java/localhost/dmoklyakov/floodfill/MainActivity.kt
@@ -2,10 +2,30 @@ package localhost.dmoklyakov.floodfill
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.project.floodfill.R
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val size = 100
+        val image = Array(size) {
+            when (it % 2) {
+                0 -> BitSet().apply {
+                    for (i in 0..size) {
+                        set(i, i%2==0)
+                    }
+                }
+                else -> BitSet().apply {
+                    for (i in 0..size) {
+                        set(i, i%2!=0)
+                    }
+                }
+            }
+        }
+        floodFillView.image = image
     }
 }
