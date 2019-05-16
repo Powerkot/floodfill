59801a378ac075a2641a9f868b6a9d9d9bcafb56
Denis Moklyakov <denis.moklyakov@ramax.ru>
2019-05-17 01:26:58 +0300

    View has been updated to match algorithm module

--- app/src/main/java/localhost/dmoklyakov/floodfill/FloodFillView.kt
+++ app/src/main/java/localhost/dmoklyakov/floodfill/FloodFillView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import java.util.*
 
@@ -12,31 +13,70 @@ import java.util.*
  */
 class FloodFillView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
 
+    interface OnTapListener {
+        fun onTapAtCell(x: Int, y: Int)
+    }
+
     private var paint = Paint()
     private var image: Array<BitSet>? = null
     private var imgSizeX = 0
     private var imgSizeY = 0
+    private var onTapListener: OnTapListener? = null
+
+    /* TODO:
+     * перерисовывать только то, что изменилось
+     * настройка каких-нибудь параметров через xml
+     * (?) запилить GLSurfaceView с рендером по запросу,
+      * сгенерить один квадрат и рисовать его многократно через glDrawArraysInstanced()
+     * (?) ИЛИ передавать кучу данных о закрашенных ячейках
+      * во фрагментный шейдер и рисовать прямоугольнички в нём
+      * (кажется, это будет сильно медленнее)
+     */
 
     init {
         paint.isAntiAlias = false
         paint.color = 0xFFFFFFFF.toInt()
+
+        setOnTouchListener { _, event ->
+            // TODO: определять просто тап, а не отпускание пальца (вроде GestureDetector умеет это)
+            if (event.action == MotionEvent.ACTION_UP || event.action == MotionEvent.ACTION_POINTER_UP) {
+                onTapListener?.onTapAtCell(
+                    (event.x / width * imgSizeX).toInt(),
+                    (event.y / height * imgSizeY).toInt()
+                )
+            }
+            return@setOnTouchListener true
+        }
     }
 
     fun setImage(image: Array<BitSet>, sizeX: Int, sizeY: Int) {
+        // TODO: кидать исключение, если x или y < 1
         this.image = image
         this.imgSizeX = sizeX
         this.imgSizeY = sizeY
+        invalidate()
+    }
+
+    fun setOnTapListener(onTapListener: OnTapListener) {
+        this.onTapListener = onTapListener
     }
 
     override fun onDraw(canvas: Canvas) {
         canvas.drawColor(0xFF000000.toInt())
+        paint.color = 0xFFFFFFFF.toInt()
         image?.let {
             val cellSizeX = 1f * width / imgSizeX
             val cellSizeY = 1f * height / imgSizeY
             for (y in 0 until imgSizeY) {
                 for (x in 0 until imgSizeX) {
                     if (it[y][x]) {
-                        canvas.drawRect(x * cellSizeX, y * cellSizeY, (x + 1) * cellSizeX, (y + 1) * cellSizeY, paint)
+                        canvas.drawRect(
+                            x * cellSizeX,
+                            y * cellSizeY,
+                            (x + 1) * cellSizeX,
+                            (y + 1) * cellSizeY,
+                            paint
+                        )
                     }
                 }
             }
--- app/src/main/java/localhost/dmoklyakov/floodfill/MainActivity.kt
+++ app/src/main/java/localhost/dmoklyakov/floodfill/MainActivity.kt
@@ -1,25 +1,70 @@
 package localhost.dmoklyakov.floodfill
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.project.algorithm.AlgorithmType
+import com.project.algorithm.Interaction
+import com.project.algorithm.InteractionImpl
+import com.project.algorithm.LogicCallback
 import com.project.floodfill.R
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FloodFillView.OnTapListener {
+
+    /* TODO:
+     * сверстать экран
+     * разная разметка ландшафт/портрет
+     * сохранять данные при повороте экрана
+     * ограничивать максимальный размер для генерации (придумать критерии для ограничения)
+     * запилить MVP, но без библиотек, а ручками
+     * решить что делать, если алгоритм отрабатывает слишком медленно (маловероятно, но мало ли)?
+     * (?) дать юзеру возможность указать seed для генерации
+     */
+
+    private var isWorkCompleted = false
+    private lateinit var interaction: Interaction
+    private var sizeX = 3
+    private var sizeY = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sizeX = 3
-        val sizeY = 3
-        val image = Array(sizeY) {
-            BitSet().apply {
-                for (j in 0 until sizeX) {
-                    set(j, Math.random() <= 0.5)
+        interaction = InteractionImpl().apply { generate(sizeX, sizeY) }
+        floodFillView.setImage(interaction.getImage(), sizeX, sizeY)
+        floodFillView.setOnTapListener(this)
+    }
+
+    override fun onTapAtCell(x: Int, y: Int) {
+        // TODO: return, если в работе
+        interaction.start(x, y, AlgorithmType.SIMPLE_FOUR_LINKED, object : LogicCallback {
+            override fun onCellChanged(x: Int, y: Int) {
+                floodFillView.setImage(
+                    interaction.getImage().apply { get(y).set(x, !(get(y).get(x))) }, // TODO: пока нет алгоритма, просто инвертирую ячейку
+                    sizeX,
+                    sizeY
+                )
+
+                Toast.makeText(this@MainActivity, "onCellChanged", Toast.LENGTH_SHORT).show()
+                GlobalScope.launch(Dispatchers.Main) {
+                    while (!isWorkCompleted) {
+                        delay(500) // TODO: вычесть время выполнения
+                        if (!isWorkCompleted) {
+                            interaction.next()
+                        }
+                    }
                 }
             }
-        }
-        floodFillView.setImage(image, sizeX, sizeY)
+
+            override fun onWorkCompleted() {
+                isWorkCompleted = true
+                Toast.makeText(this@MainActivity, "onWorkCompleted", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
+
 }
