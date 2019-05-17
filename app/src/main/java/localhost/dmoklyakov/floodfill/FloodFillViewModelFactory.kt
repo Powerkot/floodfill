1f17fea7566ba587eafd9bef3fd43951296189bc
Denis Moklyakov <denis.moklyakov@ramax.ru>
2019-05-17 16:17:58 +0300

    App module is using ViewModel + LiveData now

---
+++ app/src/main/java/localhost/dmoklyakov/floodfill/FloodFillViewModelFactory.kt
@@ -0,0 +1,12 @@
+package localhost.dmoklyakov.floodfill
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class FloodFillViewModelFactory(var width: Int, var height: Int) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return FloodFillAlgorithmViewModel(width, height) as T // да-да, unchecked cast
+    }
+
+}
--- app/src/main/java/localhost/dmoklyakov/floodfill/MainActivity.kt
+++ app/src/main/java/localhost/dmoklyakov/floodfill/MainActivity.kt
@@ -1,70 +1,44 @@
 package localhost.dmoklyakov.floodfill
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.project.algorithm.AlgorithmType
-import com.project.algorithm.Interaction
-import com.project.algorithm.InteractionImpl
-import com.project.algorithm.LogicCallback
-import com.project.floodfill.R
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import java.util.*
 
 class MainActivity : AppCompatActivity(), FloodFillView.OnTapListener {
 
     /* TODO:
      * сверстать экран
      * разная разметка ландшафт/портрет
-     * сохранять данные при повороте экрана
      * ограничивать максимальный размер для генерации (придумать критерии для ограничения)
-     * запилить MVP, но без библиотек, а ручками
      * решить что делать, если алгоритм отрабатывает слишком медленно (маловероятно, но мало ли)?
      * (?) дать юзеру возможность указать seed для генерации
+     * СДЕЛАТЬ В КОДЕ КРАСИВО
      */
 
-    private var isWorkCompleted = false
-    private lateinit var interaction: Interaction
-    private var sizeX = 3
-    private var sizeY = 3
+    private lateinit var floodFillAlgorithmViewModel: FloodFillAlgorithmViewModel
+
+    private var imageObserver = androidx.lifecycle.Observer<Array<BitSet>> {
+        floodFillView.setImage(it, sizeX, sizeY)
+    }
+
+    private var sizeX = 300
+    private var sizeY = 300
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        interaction = InteractionImpl().apply { generate(sizeX, sizeY) }
-        floodFillView.setImage(interaction.getImage(), sizeX, sizeY)
+        setContentView(com.project.floodfill.R.layout.activity_main)
         floodFillView.setOnTapListener(this)
+
+        floodFillAlgorithmViewModel =
+            ViewModelProviders.of(this, FloodFillViewModelFactory(sizeX, sizeY))
+                .get(FloodFillAlgorithmViewModel::class.java)
+        floodFillAlgorithmViewModel.imageLiveData.observe(this, imageObserver)
     }
 
     override fun onTapAtCell(x: Int, y: Int) {
-        // TODO: return, если в работе
-        interaction.start(x, y, AlgorithmType.SIMPLE_FOUR_LINKED, object : LogicCallback {
-            override fun onCellChanged(x: Int, y: Int) {
-                floodFillView.setImage(
-                    interaction.getImage().apply { get(y).set(x, !(get(y).get(x))) }, // TODO: пока нет алгоритма, просто инвертирую ячейку
-                    sizeX,
-                    sizeY
-                )
-
-                Toast.makeText(this@MainActivity, "onCellChanged", Toast.LENGTH_SHORT).show()
-                GlobalScope.launch(Dispatchers.Main) {
-                    while (!isWorkCompleted) {
-                        delay(500) // TODO: вычесть время выполнения
-                        if (!isWorkCompleted) {
-                            interaction.next()
-                        }
-                    }
-                }
-            }
-
-            override fun onWorkCompleted() {
-                isWorkCompleted = true
-                Toast.makeText(this@MainActivity, "onWorkCompleted", Toast.LENGTH_SHORT).show()
-            }
-        })
+        floodFillAlgorithmViewModel.start(x, y)
     }
 
 }
