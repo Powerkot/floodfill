0ac345c8fee26f200f7bb5a4556d1c58851d66e1
Вадим Кравченко <555lance@gmail.com>
2019-05-18 12:46:04 +0300

    Добавлен простой четырехсвязанный алгоритм

--- algorithm/src/main/java/com/project/algorithm/InteractionImpl.kt
+++ algorithm/src/main/java/com/project/algorithm/InteractionImpl.kt
@@ -4,8 +4,6 @@ import java.util.*
 
 class InteractionImpl : Interaction {
 
-    private var testCounter = 0
-
     private lateinit var image: Array<BitSet>
     private var imgSizeX = -1
     private var imgSizeY = -1
@@ -15,13 +13,16 @@ class InteractionImpl : Interaction {
     private lateinit var algorithmType: AlgorithmType
     private lateinit var logicCallback: LogicCallback
 
+    private val stack = Stack<Cell>()
+
     override fun generate(imgSizeX: Int, imgSizeY: Int) {
         this.imgSizeX = imgSizeX
         this.imgSizeY = imgSizeY
         this.image = Array(imgSizeY) {
             BitSet().apply {
                 for (j in 0 until imgSizeX) {
-                    set(j, Math.random() <= 0.5)
+                    set(j, false) //TODO временно все false для удобной отладки
+//                    set(j, Math.random() <= 0.5)
                 }
             }
         }
@@ -36,19 +37,41 @@ class InteractionImpl : Interaction {
         this.startY = y
         this.algorithmType = algorithmType
         this.logicCallback = logicCallback
+        this.stack.add(
+            Cell(
+                startX,
+                startY,
+                image[startX][startY]
+            )
+        ) //Записываем в стек элемент, по которому было совершено нажатие
         next() //выполнение первого шага
     }
 
     override fun next(): Array<BitSet> {
         when (algorithmType) {
             AlgorithmType.SIMPLE_FOUR_LINKED -> {
-                run { //TODO удалить после отладки
-                    testCounter++
-                    if (testCounter > 3) {
-                        logicCallback.onWorkCompleted()
-                    } else {
-                        logicCallback.onCellChanged(0,0)
+                if (stack.isNotEmpty()) {
+                    val cell = stack.pop()
+                    image[cell.x][cell.y] = !cell.isShaded
+                    //Проверка соседа справа
+                    if (cell.x + 1 < imgSizeX && image[cell.x + 1][cell.y] != !cell.isShaded) {
+                        stack.push(Cell(cell.x + 1, cell.y, image[cell.x + 1][cell.y]))
                     }
+                    //Проверка соседа снизу
+                    if (cell.y + 1 < imgSizeY && image[cell.x][cell.y + 1] != !cell.isShaded) {
+                        stack.push(Cell(cell.x, cell.y + 1, image[cell.x][cell.y + 1]))
+                    }
+                    //Проверка соседа слева
+                    if (cell.x - 1 >= 0 && image[cell.x - 1][cell.y] != !cell.isShaded) {
+                        stack.push(Cell(cell.x - 1, cell.y, image[cell.x - 1][cell.y]))
+                    }
+                    //Проверка соседа сверху
+                    if (cell.y - 1 >= 0 && image[cell.x][cell.y - 1] != !cell.isShaded) {
+                        stack.push(Cell(cell.x, cell.y - 1, image[cell.x][cell.y - 1]))
+                    }
+                    logicCallback.onCellChanged()
+                } else {
+                    logicCallback.onWorkCompleted()
                 }
             }
             AlgorithmType.TWO -> {
@@ -65,6 +88,6 @@ class InteractionImpl : Interaction {
 }
 
 interface LogicCallback {
-    fun onCellChanged(x: Int, y: Int)
+    fun onCellChanged()
     fun onWorkCompleted()
 }
